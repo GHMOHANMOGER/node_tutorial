acfb0f853490ad44f94cf1f07495e6e9d4dc6966
Mohan.Moger.26017998 <Mohan.Moger@microfocus.com>
2023-02-11 18:48:35 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+// Globals -No window
+
+// __dirname - Path to current directory
+// __filename - file name
+// require -  function to use modules(CommanJS)
+// module - info about the current module(file)
+// process -  info about the env where the program being executed
+
+console.log(__dirname);
+console.log(__filename);
+//console.log(require);
+// console.log(module);
+ // console.log(process);
---
+++ 4-names.js
@@ -0,0 +1,9 @@
+// local
+const secret = 'SUPER SECRET';
+
+// share
+const mohan = 'Moahn';
+const moger = 'Moger';
+
+module.exports = {mohan,moger}
+
